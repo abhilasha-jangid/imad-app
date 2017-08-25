90f8dcd7dfd918aca623de202a7712dc8665e76c
abhilasha-jangid <abhilashajangid786@gmail.com>
2017-08-25 10:23:59 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -189,7 +189,7 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/articles/:articleName' , function(req ,res)
 {
 // select * from article where title = 'article-one'
- pool.query("select * from article where title = '" + req.param.articleName + "'" ,function(err,result){
+ pool.query("select * from article where title = '" + req.params.articleName + "'" ,function(err,result){
      if(err){
          res.status(500).send(err.toString());
      }else{

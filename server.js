90595c7b09962d5c8d729d2f848e26ccb535382a
abhilasha-jangid <abhilashajangid786@gmail.com>
2017-08-25 07:12:22 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ app.get('/test-db',function(req,res){
            res.status(500).send(err.toString());
        }
        else{
-           res.send(JSON.strinify(result.rows));
+           res.send(JSON.stringify(result.rows));
        }
        
    });

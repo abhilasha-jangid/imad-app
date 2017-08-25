c82086ae31543230779b7d0011ac3581537cf88e
abhilasha-jangid <abhilashajangid786@gmail.com>
2017-08-25 10:31:40 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -155,8 +155,6 @@ app.get('/', function (req, res) {
  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-
-
 var counter = 0;
 app.get('/counter' , function (req , res)
 {
@@ -194,7 +192,7 @@ app.get('/articles/:articleName' , function(req ,res)
          res.status(500).send(err.toString());
      }else{
          if(result.rows.length === 0){
-             res.status(404).send('article not found');
+             res.status(404).send('Article not found');
          }else{
              var articleData = result.rows[0];
              res.send(createTemplate(articleData));

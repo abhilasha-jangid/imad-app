a7fb4aaedb471becb181f15c77403f36c436b7ef
abhilasha-jangid <abhilashajangid786@gmail.com>
2017-08-25 06:40:03 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = require('pg').pool;
 
+var config ={
+    user: 'abhilasha8',
+    database: 'abhilasha8',
+    host:'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 var app = express();
 app.use(morgan('dev'));
 
@@ -129,6 +137,21 @@ app.get('/', function (req, res) {
  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var pool = new pool(config);
+app.get('/test-db',function(req,res){
+   //make a select request
+   //return a response with the result
+   pool.query('select * from test',function(err,result){
+       if(err){
+           res.status(500).send(err.toString());
+       }
+       else{
+           res.send(JSON.strinify(result));
+       }
+       
+   });
+   
+});
 
 var counter = 0;
 app.get('/counter' , function (req , res)

10a3fce2150ba959f459ecc9b0e1fa4219e4593d
abhilasha-jangid <abhilashajangid786@gmail.com>
2017-08-25 06:58:36 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -133,10 +133,6 @@ function createTemplate(data) {
  return htmltemplate;
 }
 
-app.get('/', function (req, res) {
- res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
 var pool = new Pool(config);
 app.get('/test-db',function(req,res){
    //make a select request
@@ -152,6 +148,12 @@ app.get('/test-db',function(req,res){
    });
    
 });
+
+app.get('/', function (req, res) {
+ res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+
 
 var counter = 0;
 app.get('/counter' , function (req , res)
